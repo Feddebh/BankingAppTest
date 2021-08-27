46c0a9b7b0e9882d19dcc34f06b7d85e40d438bc
Feddebh <bluisfederico@gmail.com>
2021-08-27 17:27:54 -0300

    added BankAccount class with its characteristics

--- BankApplication/src/main/java/BankingApplication.java
+++ BankApplication/src/main/java/BankingApplication.java
@@ -1,5 +1,7 @@
 public class BankingApplication {
     public static void main(String[] args) {
+        BankAccount cliente1 = new BankAccount("XYZ", "0123" );
+        cliente1.showMenu();
 
     }
 }
